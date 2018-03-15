fc8b501704805c0767b5a58b55a8a941e3fa8d61
Sergowa <bonuk.mail@gmail.com>
2018-03-15 16:29:01 +0200

    Adding user_id to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ApplicationRecord
+	
+	belongs_to :user
+
 	#Картинки к посту
-	has_attached_file :image, styles: { medium: "400x400#"}
+	has_attached_file :image, styles: { medium: "500x500>"}
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 	#Проверка на заполненость поста
