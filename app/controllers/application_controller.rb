69d7d3a6636d2a00d9858807f5ae3565fb17bafa
Sergowa <bonuk.mail@gmail.com>
2018-03-15 11:57:23 +0200

    Adding phones & names to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+
+  # For Saving name and phones/ For Devise/ Adding by Sergowa
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:phone])
+  end
+
 end
