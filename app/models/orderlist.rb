3d3280d45a0bb2aa614f240e327b0241e3fd24e2
Vladimir Belich <vladimir.belich@gmail.com>
2018-03-15 12:17:04 +0200

    Added order review to master

--- app/models/orderlist.rb
+++ app/models/orderlist.rb
@@ -6,7 +6,7 @@ class OrderList < ActiveRecord::Base
 
 		ms_sql = ActiveRecord::Base
 
-		@results = ms_sql.connection.select_all("SELECT _Number, DATEADD(YEAR, -2000, _Date_Time) AS _Date_Time FROM _Document302 WHERE (_number = 'Э0000008434' OR _number = 'Э0000008435' OR _number = 'Э0000008436') AND _Date_Time > '4018-01-01 00:00:00' ").to_hash
+		@results = ms_sql.connection.select_all("SELECT _Number, DATEADD(YEAR, -2000, _Date_Time) AS _Date_Time FROM _Document302 WHERE (_number = 'Э0000008434' OR _number = 'Э0000008435' OR _number = 'Э0000008436' OR _number = 'Э0000009036') AND _Date_Time > '4018-01-01 00:00:00' ").to_hash
 
 		return @results
 		
