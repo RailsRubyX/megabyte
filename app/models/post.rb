c9ac72e7931b9983878a3810c7e8fbc8dd51d34c
Sergowa <bonuk.mail@gmail.com>
2018-03-16 13:35:20 +0200

    Styling blog index, show

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
 	belongs_to :user
 
 	#Картинки к посту
-	has_attached_file :image, styles: { medium: "500x500>"}
+	has_attached_file :image, styles: { medium: "1170x900>", :thumb => "360x300>"}
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 	#Проверка на заполненость поста
